7dd43fa6e2d5ab187db6ce4ea5bb58afa980d66f
Amit <amithen48@gmail.com>
2022-03-04 17:10:57 +0200

    commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,6 @@ import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
 import { projects } from "../data";
 
-// import pic from "../static/dijkstras_vis.gif";
-
 function Projects() {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
@@ -13,11 +11,7 @@ function Projects() {
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Apps I've Built
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
-          </p>
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base"></p>
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
@@ -29,11 +23,10 @@ function Projects() {
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  // src={project.image}
+                  className="absolute inset-0 w-full h-full object-center object-fit: fill"
                   src={require("../static/" + project.image + ".gif")}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-20 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
--- src/data.js
+++ src/data.js
@@ -1,38 +1,44 @@
-import pic from "./static/shalom.png";
-
 export const projects = [
-  {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "dijkstras_vis",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "dijkstras_vis",
-    link: "https://reedbarger.com",
-  },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "dijkstras_vis",
-    link: "https://jsbootcamp.com",
-  },
   {
     title: "Epic Todo App",
     subtitle: "React Hooks",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "dijkstras_vis",
+      "Firebase app, using React and Firebase, creating UI for manging user orders and integrating with DB to save user orders and displaying them to the user.",
+    image: "burger_builder",
     link: "https://pythonbootcamp.com",
+  },
+  {
+    title: "Memory Game App",
+    subtitle: "React and Firebase",
+    description:
+      "Firebase app, using React (Hooks, Redux, Router), Node.js, Firebase. creating a memory game Managing users progress, integrating with DB to create a full scale app.",
+    image: "memory_game",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "Audio Cool",
+    subtitle: "Vanilla JS",
+    description:
+      "Pure JS app, creating a soundboard with nice animations using CnavasJS library",
+    image: "audio_cool",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Dijkstra Visualizer",
+    subtitle: "React Hooks",
+    description:
+      "React app using hooks, visualizing the way Dijkstra's algorithm solves the shortest path problem on a weighted graph.",
+    image: "dijkstras_vis",
+    link: "https://reactbootcamp.com",
   },
 ];
 
-export const skills = ["React.js", "C/C++", "Java", "JavaScript", "Python"];
+export const skills = [
+  "React.js",
+  "C / C++",
+  "Java",
+  "JavaScript",
+  "Python",
+  "SQL",
+  "Node.js",
+];
